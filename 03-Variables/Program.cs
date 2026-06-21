268d44f41c7bb96181a8d7ab6b7e55bce090e4c6
zizadhrmaa <zizadhrmaaa@gmail.com>
2026-06-22 04:32:14 +0700

    feat: constants program

--- 03-Variables/Program.cs
+++ 03-Variables/Program.cs
@@ -1,4 +1,5 @@
-﻿int Angka = 5;
+﻿// Variables
+int Angka = 5;
 Angka = 27;
 string Namaku = "Ziza";
 char Huruf = 'A';
@@ -6,6 +7,7 @@ bool UdahMamLum = false;
 double DollarToRupiah = 17811.10;
 decimal RupiahToDollar = 0.000056m;
 
+Console.WriteLine("== Section Variables == ");
 Console.WriteLine(Angka);
 Console.WriteLine(Namaku);
 Console.WriteLine(Huruf);
@@ -14,3 +16,11 @@ Console.WriteLine(DollarToRupiah);
 Console.WriteLine(DollarToRupiah.ToString("F2"));
 Console.WriteLine(RupiahToDollar);
 Console.WriteLine($"{RupiahToDollar:F6}");
+Console.WriteLine("");
+
+//Constants
+const double Pi = 3.141592653589793;
+// gaboleh Pi = angka lain, bakal error
+// gaboleh deklarasi konstanta tapi assign value
+Console.WriteLine("== Section Constants == ");
+Console.WriteLine(Pi);
