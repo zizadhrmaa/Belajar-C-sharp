aa89dd8e3f964d3ce263279256b8eb43ee32d331
zizadhrmaa <zizadhrmaaa@gmail.com>
2026-06-22 04:20:51 +0700

    feat: variables program

---
+++ 03-Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿int Angka = 5;
+Angka = 27;
+string Namaku = "Ziza";
+char Huruf = 'A';
+bool UdahMamLum = false;
+double RupiahToDollar = 0.000056;
+
+Console.WriteLine(Angka);
+Console.WriteLine(Namaku);
+Console.WriteLine(Huruf);
+Console.WriteLine(UdahMamLum);
+Console.WriteLine(RupiahToDollar);
+Console.WriteLine(RupiahToDollar.ToString("F6"));
