ac0412f3528cbd466f2390d14b430a3db922b1dd
zizadhrmaa <zizadhrmaaa@gmail.com>
2026-06-22 04:26:39 +0700

    feat: type decimal

--- 03-Variables/Program.cs
+++ 03-Variables/Program.cs
@@ -3,11 +3,14 @@ Angka = 27;
 string Namaku = "Ziza";
 char Huruf = 'A';
 bool UdahMamLum = false;
-double RupiahToDollar = 0.000056;
+double DollarToRupiah = 17811.10;
+decimal RupiahToDollar = 0.000056m;
 
 Console.WriteLine(Angka);
 Console.WriteLine(Namaku);
 Console.WriteLine(Huruf);
 Console.WriteLine(UdahMamLum);
+Console.WriteLine(DollarToRupiah);
+Console.WriteLine(DollarToRupiah.ToString("F2"));
 Console.WriteLine(RupiahToDollar);
-Console.WriteLine(RupiahToDollar.ToString("F6"));
+Console.WriteLine($"{RupiahToDollar:F6}");
