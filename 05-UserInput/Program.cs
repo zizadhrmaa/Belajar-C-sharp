a73ad148454d04d16ee060fdb56dd8e7b0643dd6
zizadhrmaa <zizadhrmaaa@gmail.com>
2026-06-23 18:40:01 +0700

    feat: user input program

---
+++ 05-UserInput/Program.cs
@@ -0,0 +1,13 @@
+﻿// User Input pake fungsi Console.ReadLine() dengan hasil input PASTI string
+// Kalau mau input bukan string tapi tipe data lain, tinggal casting aja
+Console.WriteLine("Masukkan usernamemu");
+string myUsername = Console.ReadLine();
+Console.WriteLine("Masukkan umurmu");
+int myAge = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Masukkan status pernikahanmu");
+bool myStatus = Convert.ToBoolean(Console.ReadLine()); //riskan sih, cara ini cuma buat belajar casting
+Console.WriteLine("");
+Console.WriteLine("== Hasil ==");
+Console.WriteLine("Username    : " + myUsername);
+Console.WriteLine("Umur        : " + myAge);
+Console.WriteLine("Udah nikah? : " + myStatus);
