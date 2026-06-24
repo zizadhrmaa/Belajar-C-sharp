2145547affe9037818f0628f98aee53234a1882e
zizadhrmaa <zizadhrmaaa@gmail.com>
2026-06-24 23:57:11 +0700

    feat: operators aritmatika

---
+++ 06-Operators/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("== Operators ==");
+Console.WriteLine("Aritmatika");
+int a = 10;
+int b = 5;
+int c = 2;
+int d = 3;
+
+Console.WriteLine("a = " + a + " | b = " + b + " | c = " + c + " | d = " + d);
+Console.WriteLine("a+b = " + Convert.ToInt32(a+b));
+Console.WriteLine("b-c = " + Convert.ToInt32(b-c));
+Console.WriteLine("c*d = " + Convert.ToInt32(c*d));
+Console.WriteLine("a/2 = " + Convert.ToInt32(a/2));
+Console.WriteLine("6%4 = " + Convert.ToInt32(6%4));
+d++;
+Console.WriteLine("d++ = " + Convert.ToInt32(d));
+d--;
+Console.WriteLine("d-- = " + Convert.ToInt32(d));
