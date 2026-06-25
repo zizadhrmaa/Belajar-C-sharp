41b7bcf069113b7e73e3016444ab92ed47f7b923
zizadhrmaa <zizadhrmaaa@gmail.com>
2026-06-26 01:15:44 +0700

    feat: loops program

---
+++ 10-Loops/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("While loop");
+int i = 5;
+while (i > 0) {
+    Console.Write("  ");
+    Console.WriteLine(i); 
+    i--;
+}
+
+Console.WriteLine("");
+Console.WriteLine("Do/While loop");
+int j = 1;
+do {
+    Console.Write("  ");
+    Console.WriteLine(j);
+    j++;
+} while (j <= 5);
+
+Console.WriteLine("");
+Console.WriteLine("For loop");
+for (int k = 1; k <= 5; k++) {
+    Console.Write("  ");
+    Console.WriteLine(k);
+}
+
+Console.WriteLine("");
+Console.WriteLine("Nested For loop");
+for (int m = 1; m <= 5; m++) {
+    Console.Write("  ");
+    Console.WriteLine("luar: " + m);
+    for (int n = 5; n >= 1; n--) {
+        Console.Write("    ");
+        Console.WriteLine("dalam: " + n);
+    }
+}
