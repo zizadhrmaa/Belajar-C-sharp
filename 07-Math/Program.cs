72b4e193708b535ce125bfea50651f33365a9cc0
zizadhrmaa <zizadhrmaaa@gmail.com>
2026-06-25 23:24:05 +0700

    feat: math function

---
+++ 07-Math/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Fungsi Math");
+Console.WriteLine(Math.Max(5, 10));
+Console.WriteLine(Math.Min(5, 10));
+
+//diakarkan
+Console.WriteLine(Math.Sqrt(8));
+
+//nilai mutlak
+Console.WriteLine(Math.Abs(-33.9));
+
+//dibulatkan
+Console.WriteLine(Math.Round(-33.9));
